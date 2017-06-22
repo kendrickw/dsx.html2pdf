327d9e65d42bc91a2bfd7c48436e8aaefc684bf7
Kendrick Wong <kendrickw@gmail.com>
2017-06-21 23:22:46 -0400

    use wkhtmltopdf

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
 const cheerio = require('cheerio')
 const pdf = require('html-pdf')
 const fs = require('fs');
-
-// Default option arguments
-width = '8in'
-height = '10.5in'
+const child_process = require('child_process');
 
 function usage () {
     console.log('Usage:')
-    console.log('  ' + process.argv0 + ' <input> <output> [width] [height]')
+    console.log('  ' + process.argv0 + ' <input> <output>')
     console.log('Where:')
     console.log('   input: input HTML file name')
     console.log('  output: output PDF file name')
-    console.log('   width: (optional) page width (mm, cm, in or px).  Default ' + width)
-    console.log('  height: (optional) page height (mm, cm, in or px).  Default ' + height)
     console.log('Example:')
-    console.log('  ' + process.argv0 + ' Q1.html Q1.pdf 20in 16in')
+    console.log('  ' + process.argv0 + ' Q1.html Q1.pdf')
+    console.log('NOTE: this tool require installation of wkhtmltopdf (https://wkhtmltopdf.org)')
     process.exit(-1) 
-}
-
-function validateUnit (input) {
-    if (input.endsWith('mm') || input.endsWith('cm') || input.endsWith('in') || input.endsWith('px')) {
-        return input
-    }
-    usage()
 }
 
 // process input arguments
 if (process.argv.length < 4) {
   console.error('Must provide a input html file and a output PDF file!')
   usage()
-} else if (process.argv.length === 5) {
-  width = validateUnit(process.argv[4])
-} else if (process.argv.length === 6) {
-  width = validateUnit(process.argv[4])
-  height = validateUnit(process.argv[5])
 } else if (process.argv.length !== 4){
   usage()
 }
 
 var inputfile = process.argv[2]
 var outputfile = process.argv[3]
+const temphtmlfile = 'temp.html'
 
 // strip control characters from filenames
 outputfile = outputfile.replace(/[^\x20-\x7E]+/g, '')
@@ -72,6 +57,17 @@ const pageBreak = [
 const tableStyles = [
     'table {',
     '  border-collapse: collapse;',
+    '  page-break-inside: auto;',
+    '}',
+    'tr {',
+    '  page-break-inside: avoid;',
+    '  page-break-after: auto;',
+    '}',
+    'thead {',
+    '  display: table-header-group',
+    '}',
+    'tfoot {',
+    '  display: table-footer-group',
     '}',
     'table, th, td {',
     '  border: 1px solid black;',
@@ -90,12 +86,14 @@ const tocList = []
 $('*').each(function(i, elem) {
     if (elem.name === 'h1') {
         const anchorName = 'h1_' + i
-        $(elem).prepend('<a name="' + anchorName + '"></a>')
+        const anchor = `<a name="${anchorName}">&nbsp;</a>`
+        $(elem).before(anchor)
         $(elem).addClass('page-break')
         tocList.push('<li><a href="#' + anchorName + '">' + $(elem).text() + '</a></li>')
     } else if (elem.name === 'h2') {
         const anchorName = 'h2_' + i
-        $(elem).prepend('<a name="' + anchorName + '"></a>')
+        const anchor = `<a name="${anchorName}">&nbsp;</a>`
+        $(elem).before(anchor)
         tocList.push('<ul><li><a href="#' + anchorName + '">' + $(elem).text() + '</a></li></ul>')
     }
 })
@@ -112,23 +110,28 @@ $([
 const html = $.html()
 
 // Write HTML to temp file
-fs.writeFileSync('temp.html', html, 'utf8')
+fs.writeFileSync(temphtmlfile, html, 'utf8')
+console.log('HTML saved to ' + temphtmlfile)
+
+child_process.execFileSync('wkhtmltopdf', ['-d', '300', '--print-media-type', temphtmlfile, outputfile])
+console.log('PDF saved to ' + outputfile);
 // process.exit(0)
 
 // Convert HTML to PDF
-const options = { 
-  // Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html 
-  "height": height,        // allowed units: mm, cm, in, px 
-  "width": width,            // allowed units: mm, cm, in, px 
-  // or
-  // "format": "Letter",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid 
-  // "orientation": "portrait", // portrait or landscape 
-}
+// const options = { 
+//   // Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html 
+//   "height": height,        // allowed units: mm, cm, in, px 
+//   "width": width,            // allowed units: mm, cm, in, px 
+//   // or
+//   // "format": "Letter",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid 
+//   // "orientation": "portrait", // portrait or landscape 
+// }
 
-pdf.create(html, options).toFile(outputfile, function(err, res) {
-  if (err) {
-    return console.log(err)
-  }
-  
-  console.log('PDF saved to ' + outputfile);    
-})
+// pdf.create(html, options).toFile(outputfile, function(err, res) {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   
+//   console.log('PDF saved to ' + outputfile);    
+// })
+
